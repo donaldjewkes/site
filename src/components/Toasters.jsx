5663bb07f2f2057097edae10f7b24331a8a0bdcf
donald <donaldjewkes@gmail.com>
2022-11-15 21:48:59 -0800

    react comp

---
+++ src/components/Toasters.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+const numToasters = React.props;
+
+export default function Toasters(props) {  
+    return (
+        <div class="pt-2">
+            <div class=" text-lg">Rating: <span class="font-bold">{props.numToasters}/10 Toasters</span></div>
+            <div class="p-1.5 bg-lime-800/50 inline-flex rounded space-x-1">
+                    {[...Array(props.numToasters)].map((star) => {        
+                    return (         
+                        <div>
+                            <img  class="h-6" src="/images/icons/btoaster.png"></img>
+                        </div>
+                    );
+                    })}
+                    {[...Array(10-props.numToasters)].map((star) => {        
+                    return (         
+                        <div>
+						    <img  class="h-6 grayscale opacity-50" src="/images/icons/btoaster.png"></img>
+                        </div>
+                    );
+                    })}
+            </div>
+        </div>
+    );
+  };
+
+
