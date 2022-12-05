a74e0d12a9be5ac031fe0184553e876907b7c502
donald <donaldjewkes@gmail.com>
2022-12-04 21:03:54 -0800

    toaster ratings

--- src/components/Toasters.jsx
+++ src/components/Toasters.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 const numToasters = React.props;
 
+
 export default function Toasters(props) {  
     return (
         <div class="pt-2">
             <div class=" text-lg">Rating: <span class="font-bold">{props.numToasters}/10 Toasters</span></div>
-            <div class="p-1.5 bg-lime-800/50 inline-flex rounded space-x-1">
+            <div class="p-1.5 bg-primary inline-flex rounded space-x-1">
                     {[...Array(props.numToasters)].map((star) => {        
                     return (         
                         <div>
